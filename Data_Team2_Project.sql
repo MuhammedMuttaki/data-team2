bb889859596c916ab5111c0bb0e7a991ada063d2
emrec21121 <emrec21121@gmail.com>
2023-01-28 18:39:19 +0300

    Progress after second week's meeting

---
+++ Data_Team2_Project.sql
@@ -0,0 +1,64 @@
+-- Exploratory analysis
+-- Task 1: Find out countries of customers, and categories sold to each one.
+SELECT COUNT(DISTINCT Country)
+FROM Customers AS c;
+
+SELECT COUNT(DISTINCT ShipCountry)
+FROM Orders AS o;
+
+SELECT c.Country, c2.CategoryName, COUNT(od.OrderID)
+FROM Customers AS c
+INNER JOIN Orders AS o
+USING (CustomerID)
+INNER JOIN "Order Details" AS od
+USING (OrderID)
+INNER JOIN Products AS p 
+USING (ProductID)
+INNER JOIN Categories AS c2
+USING (CategoryID)
+GROUP BY 1, 2;
+
+SELECT o.ShipCountry AS Countries, GROUP_CONCAT(DISTINCT c2.CategoryName) AS Categories
+FROM Orders AS o
+INNER JOIN "Order Details" AS od
+USING (OrderID)
+INNER JOIN Products AS p 
+USING (ProductID)
+INNER JOIN Categories AS c2
+USING (CategoryID)
+GROUP BY 1;
+
+-- Task 2: Yearly sales amounts for years 2016, 2017, and 2018; categorized as high, mid, and low, by country.
+
+SELECT o.ShipCountry, 
+	   CASE WHEN SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) < 7000 THEN "Low"
+			WHEN SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) < 18000 THEN "Mid"
+			ELSE "High" END AS sales_amount
+FROM "Order Details" as od
+INNER JOIN Orders as o
+USING(OrderID)
+WHERE o.OrderDate BETWEEN '2016-01-01' AND '2016-12-31'
+GROUP BY 1
+ORDER BY SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) DESC;
+
+SELECT o.ShipCountry, 
+	   CASE WHEN SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) < 7000 THEN "Low"
+			WHEN SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) < 18000 THEN "Mid"
+			ELSE "High" END AS sales_amount
+FROM "Order Details" as od
+INNER JOIN Orders as o
+USING(OrderID)
+WHERE o.OrderDate BETWEEN '2017-01-01' AND '2017-12-31'
+GROUP BY 1
+ORDER BY SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) DESC;
+
+SELECT o.ShipCountry, 
+	   CASE WHEN SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) < 7000 THEN "Low"
+			WHEN SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) < 18000 THEN "Mid"
+			ELSE "High" END AS sales_amount
+FROM "Order Details" as od
+INNER JOIN Orders as o
+USING(OrderID)
+WHERE o.OrderDate BETWEEN '2018-01-01' AND '2018-12-31'
+GROUP BY 1
+ORDER BY SUM((od.UnitPrice - (od.UnitPrice * od.Discount)) * od.Quantity) DESC;
